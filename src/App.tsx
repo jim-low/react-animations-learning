bba2736841bb85bb8ecab134e0e4101aff7d8cf3
jim-low <robil12380@gmail.com>
2025-03-08 00:10:37 +0800

    chore: clean up once more

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,15 @@ import './App.css'
 import { Tabs, Tab } from '@mui/material'
 import { PageTabs } from './types'
 import CustomTabPanel from './components/CustomTabPanel';
-import CanvasBasics from './components/CanvasBasics';
 
 const pageTabsMappings = {
-  [PageTabs.CANVAS_BASICS]: CanvasBasics,
   [PageTabs.CANVAS_CHARTS_AND_GRAPH]: undefined,
+  [PageTabs.CANVAS_GAME]: undefined,
   [PageTabs.CANVAS_ANTV_REPLICATE]: undefined,
 }
 
 function App() {
-  const [selectedTab, setSelectedTab] = useState<PageTabs>(PageTabs.CANVAS_BASICS);
+  const [selectedTab, setSelectedTab] = useState<PageTabs>(PageTabs.CANVAS_GAME);
 
   return (
     <div>
@@ -21,8 +20,8 @@ function App() {
         onChange={(_, tab) => setSelectedTab(tab)}
         className='mb-2'
       >
-        <Tab value={PageTabs.CANVAS_BASICS} label="Canvas Basics" />
         <Tab value={PageTabs.CANVAS_CHARTS_AND_GRAPH} label="Canvas Charts and Graph" />
+        <Tab value={PageTabs.CANVAS_GAME} label="Canvas Game" />
         <Tab value={PageTabs.CANVAS_ANTV_REPLICATE} label="Canvas AntV Replicate" />
       </Tabs>
 
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,10 +1,21 @@
 export enum PageTabs {
-  CANVAS_BASICS = 'CANVAS_BASICS',
   CANVAS_CHARTS_AND_GRAPH = 'CANVAS_CHARTS_AND_GRAPH',
+  CANVAS_GAME = 'CANVAS_GAME',
   CANVAS_ANTV_REPLICATE = 'CANVAS_ANTV_REPLICATE',
 };
 
 export type Vector2D = {
   x: number;
   y: number;
+};
+
+export type Dimensions2D = {
+  width: number;
+  height: number;
+};
+
+export enum EAntLogoState {
+  INIT = 'INIT',
+  EXPAND = 'EXPAND',
+  BOUNCE = 'BOUNCE',
 };
