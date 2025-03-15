613dc9ae133c9f09d51a1b03e8d93ebcc0a2ea59
jim-low <robil12380@gmail.com>
2025-03-16 00:25:29 +0800

    feat: add routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route key={'/react-animations-learning'} path={'/react-animations-learning'} element={<App />} />
+  )
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
   </StrictMode>,
 )
